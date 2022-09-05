aa309e651d3e91b59b399535a087406b4771083c
xiang song <waps163com@163.com>
2022-09-05 14:02:54 +0800

    modify

--- install_cluster.sh
+++ install_cluster.sh
@@ -3,7 +3,7 @@
 source $(dirname "${BASH_SOURCE[0]}")/set_cloud.sh
 source $(dirname "${BASH_SOURCE[0]}")/set_inventory.sh
 
-ansible-playbook "playbooks/install_cluster.yml" \
+ansible-playbook "playbooks/install_cluster.yml" -vvv \
                  --inventory="inventory/${inventory_to_use}" \
                  --extra-vars="cloud_name=${cloud_to_use}" \
                  "$@"
